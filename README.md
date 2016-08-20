771c7d71cb603c448ad92dc948a1fb81657528b7
Mogu programming <kbu94984@gmail.com>
2016-08-20 21:33:42 +0900

    INIT: add src/ setup.py,MOD: README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# BasicModule
-Fundamental methods.
+# Easy_utill (Python3 scripts)
+Easy utill provide memory usage, flush animation during calculation and so on.
+
+## Requirements
+* Python > 3.5.1
+
+## build & install
+```
+python3 setup.py build
+python3 setup.py install
+```
+
+## How to use
+```python
+import easy_utill
+
+
+```
+
+## Demo
+
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+from setuptools import setup
+
+setup(name='easy_utill',
+      version='0.0.1',
+      description='Easy utill provide memory usage, flush animation during  calculation and so on.',
+      author='Mogu',
+      author_email='kbu94984@gmail.com',
+      url='',
+      packages=['src'],
+      install_requires=[],)
